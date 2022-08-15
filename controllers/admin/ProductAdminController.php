7f21682fb9b45dfc25c37512eb17dcd4aefa64a3
TanBryant <nhattan0932507137@gmail.com>
2022-08-15 08:06:58 +0700

    first commit

---
+++ controllers/admin/ProductAdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+class ProductAdminController extends Controller
+{
+	
+	function __construct()
+	{
+		$this->folder = "admin";
+		if(!isset($_SESSION['admin'])){
+			header("Location: http://localhost:85/WBH_MVC/indexadmin");
+		}
+	}
+	function index(){
+		require_once 'vendor/Model.php';
+		require_once 'models/admin/productModel.php';
+		$md = new productModel;
+		$data = $md->getAllPrds();
+		$this->render('product',$data,'SẢN PHẨM','admin');
+	}
+}
